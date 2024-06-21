4e12fa5e882a89ef2f9c4f1d5f921ade53cce5d4
jontehruss <johann.russeau@protonmail.com>
2024-06-21 15:43:52 +0200

    update login form

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,26 +1,80 @@
 
 
-// initialiser le header de la requête HTTP
-let httpHeaders = new Headers();
-httpHeaders.append("Content-Type", "application/json");
+//  cibler le formulaire de login et ses champs
+let loginForm = document.querySelector('#login-form');
+let eMail = document.querySelector('#email');
+let passwd = document.querySelector('#pwd');
 
-// Récupérer les infos de connexion depuis le formulaire
-let id = JSON.stringify({
-  "email": "sophie.bluel@test.tld",
-  "password": "S0phie"
+// cibler le bouton de validation du formulaire
+let loginBtn = document.querySelector('input[type="submit"]');
+loginBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // console.log(eMail.value);
+  // console.log(passwd.value);
+
+  formatCredentials(eMail.value, passwd.value);
+
 });
 
 
-// initialiser la requête POST
-let requestOptions = {
-  method: 'POST',
-  headers: httpHeaders,
-  body: id,
-  redirect: 'follow'
-};
 
-// envoyer la requête de connexion
-fetch("http://127.0.0.1:5678/api/users/login", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+/**
+ * @param eMail est récupéré du champ email du formulaire
+ * @param passwd est récupéré du champ password du formulaire
+ */
+function formatCredentials(eMail, passwd) {
+
+  // Récupérer les infos de connexion depuis le formulaire
+  let id = JSON.stringify({
+    "email": eMail,
+    "password": passwd
+  });
+
+  // Création JSON manuelle avec interpollation
+    // let id = `{"email":"${eMail}","password":"${passwd}"}`;
+    // console.log(id);
+
+  // Appel de la fonction loginRequest avec l'id pour effectuer l'authentification 
+  loginRequest(id);
+}
+
+
+
+function loginRequest(id) {
+
+  // initialiser le header de la requête HTTP
+  let httpHeaders = new Headers();
+  httpHeaders.append("Content-Type", "application/json");
+  // console.log(Headers);
+
+  // définir la méthode POST et insérer l'id JSON dans le body
+  let requestOptions = {
+    method: 'POST',
+    headers: httpHeaders,
+    body: id,
+    redirect: 'follow'
+  };
+  console.log(requestOptions);
+
+  // envoyer la requête de connexion
+  // TODO - Voir pour remplacer la partie http:Domain:Port de l'url par une variable 
+  fetch("http://127.0.0.1:5678/api/users/login", requestOptions)
+    .then(response => response.text())
+
+    .then(result => console.log(result))
+
+    .catch(error => console.log('error', error));
+
+
+}
+
+
+
+
+/**
+ * TODO: 
+ * -> JSON.Parse sur result pour récupérer le token
+ * -> Utiliser le token pour se connecter
+ * ->  fonction pour vérifier si token dans le localstorage - si ok afficher le bandeau supérieur (connecté)
+ */
